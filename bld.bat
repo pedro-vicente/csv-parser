ac8c8166a9772d376b54109eaf7a8f708967c227
pedro-vicente <pedro.vicente@space-research.org>
2018-02-17 00:55:04 -0500

    build script

--- bld.bat
+++ bld.bat
@@ -1,14 +1,11 @@
 
 @echo off
 if not defined DevEnvDir (
- echo "%VS140COMNTOOLS%VsDevCmd.bat" 
- call "%VS140COMNTOOLS%VsDevCmd.bat" 
- echo "%VCINSTALLDIR%vcvarsall.bat" amd64
- call "%VCINSTALLDIR%vcvarsall.bat" amd64
+ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
  if errorlevel 1 goto :eof
 )
 
+
 cmake -H. -Bbuild -G "Visual Studio 14 2015 Win64" 
-msbuild build\read_csv.sln /target:build /property:configuration=debug
-build\Debug\read_dc311.exe -f dc_311-2016_100.csv
-build\Debug\read_test.exe -f test.csv
+msbuild build\nco_test.sln /target:build /property:configuration=debug
+build\Debug\nco_test.exe
